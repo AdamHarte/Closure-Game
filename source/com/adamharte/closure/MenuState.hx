7fe77a73ba413bd5ce88581b45f01764f9f8e1a0
AdamHarte <adam@adamharte.com>
2014-08-23 17:44:43 +1200

    Skip menu state.

--- source/com/adamharte/closure/MenuState.hx
+++ source/com/adamharte/closure/MenuState.hx
@@ -5,19 +5,31 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxMath;
+
+using flixel.util.FlxSpriteUtil;
 
 /**
  * A FlxState which can be used for the game's menu.
  */
 class MenuState extends FlxState
 {
+	var _playBtn:FlxButton;
+	
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		_playBtn = new FlxButton(0, 0, "Play", playBtnClick);
+		_playBtn.screenCenter();
+		add(_playBtn);
+		
 		super.create();
+		
+		playBtnClick(); // Skip menu for faster development.
 	}
 	
 	/**
@@ -27,6 +39,8 @@ class MenuState extends FlxState
 	override public function destroy():Void
 	{
 		super.destroy();
+		
+		_playBtn = FlxDestroyUtil.destroy(_playBtn);
 	}
 
 	/**
@@ -35,5 +49,12 @@ class MenuState extends FlxState
 	override public function update():Void
 	{
 		super.update();
-	}	
+	}
+	
+	
+	
+	function playBtnClick() 
+	{
+		FlxG.switchState(new PlayState());
+	}
 }
--- source/com/adamharte/closure/PlayState.hx
+++ source/com/adamharte/closure/PlayState.hx
@@ -5,18 +5,26 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxMath;
+
+using flixel.util.FlxSpriteUtil;
 
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
+	
+	
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		
+		
 		super.create();
 	}
 	
@@ -35,5 +43,5 @@ class PlayState extends FlxState
 	override public function update():Void
 	{
 		super.update();
-	}	
+	}
 }
